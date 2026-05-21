0f4b6a787ac193ba0ce14e4a5692a8dfe4ca508c
moritz <moritz.siebert@htlstp.at>
2026-05-21 20:07:11 +0200

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {EmployeeList} from './features/employees/employee-list/employee-list';
+import {EmployeeCreate} from './features/employees/employee-create/employee-create';
+import {EmployeeDetail} from './features/employees/employee-detail/employee-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {path: '', component: EmployeeList},
+  {path: 'employees/create', component: EmployeeCreate},
+  {path: 'employees/:id', component: EmployeeDetail}
+];
