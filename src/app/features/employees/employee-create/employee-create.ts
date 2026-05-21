fcc10de066cac93b72a61d30bf5d62390481cb11
moritz <moritz.siebert@htlstp.at>
2026-05-21 20:06:36 +0200

    added employee create page

---
+++ src/app/features/employees/employee-create/employee-create.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, Router } from '@angular/router';
+import { EmployeeService } from '../../../core/services/employee.service';
+import { Employee } from '../../../core/models/employee';
+
+@Component({
+  selector: 'app-employee-create',
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterLink
+  ],
+  templateUrl: './employee-create.html',
+  styleUrl: './employee-create.css',
+})
+export class EmployeeCreate {
+  id = '';
+  firstName = '';
+  lastName = '';
+
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router
+  ) {}
+
+  save() {
+    const employee: Employee = {
+      id: this.id,
+      firstName: this.firstName,
+      lastName: this.lastName
+    };
+
+    if (employee.id && employee.firstName && employee.lastName) {
+      this.employeeService.create(employee).subscribe(
+        (data) => {
+          console.log('Employee created successfully:', data);
+          this.router.navigate(['/']);
+        },
+        (error) => {
+          console.error('Error creating employee:', error);
+        }
+      );
+    } else {
+      alert('Please fill in all fields');
+    }
+  }
+}
